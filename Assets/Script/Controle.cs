a62250b3a0cfe88c416f0d3e05863bf0b14066a3
AuraSaile <antoinecorriveau2005@gmail.com>
2025-02-24 13:42:12 -0500

    Gros problème

--- Assets/Script/Controle.cs
+++ Assets/Script/Controle.cs
@@ -2,15 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Windows;
 
 public class Controle : MonoBehaviour
 {
 
-    [SerializeField] private float _vitesse;
+    // variables de mouvement et contrôle
+    [SerializeField] private float _vitessePromenade;
     private Rigidbody _rb;
     private Vector3 directionInput;
 
-    [SerializeField] private float _modifierAnimation;
+    // variables de contrôle d'animation
+    [SerializeField] private float _modifierAnimTranslation;
     private Animator _animator;
     private float _rotationVelocity;
 
@@ -21,25 +24,21 @@ public class Controle : MonoBehaviour
         _animator = GetComponent<Animator>();
     }
 
+    void OnPromener(InputValue directionBase)
+    {
+        Vector3 directionAvecVitesse = directionBase.Get<Vector3>() * _vitessePromenade;
+        directionInput = new Vector3(0f,directionAvecVitesse.z , directionAvecVitesse.y);
+    }
+
+    void FixedUpdate()
+    {
+        // calculer et appliquer la translation
+        Vector3 mouvement = directionInput;
+    }
+
     // Update is called once per frame
     void Update()
     {
         
-    }
-
-    void OnAvancerReculer(InputValue directionBase) 
-    {
-        Debug.Log("Yo");
-        Vector2 directionAvecVitesse = directionBase.Get<Vector2>() * _vitesse;
-        directionInput = new Vector3(0f, directionAvecVitesse.x, directionAvecVitesse.y);
-    }
-
-    private void FixedUpdate()
-    {
-        Vector3 mouvement = directionInput;
-
-        _rb.AddForce(mouvement, ForceMode.VelocityChange);
-
-        Vector3 vitesseSurPlane = new Vector3(_rb.velocity.x, 0f, _rb.velocity.z);
     }
 }
