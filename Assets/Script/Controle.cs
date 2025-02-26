758bd1b04b8ca73a74ea9ce7497992b920e5a8c7
AuraSaile <antoinecorriveau2005@gmail.com>
2025-02-26 18:42:01 -0500

    Animation début

--- Assets/Script/Controle.cs
+++ Assets/Script/Controle.cs
@@ -9,6 +9,8 @@ public class Controle : MonoBehaviour
 
     // variables de mouvement et contrôle
     [SerializeField] private float _vitessePromenade;
+    [SerializeField] private float _MultiplicateurA;
+    [SerializeField] private float _MultiplicateurB;
     private Rigidbody _rb;
     private Vector3 directionInput;
 
@@ -22,18 +24,27 @@ public class Controle : MonoBehaviour
     {
         _rb = GetComponent<Rigidbody>();
         _animator = GetComponent<Animator>();
+        _MultiplicateurA = -1;
+        _MultiplicateurB = -1;
     }
 
     void OnPromener(InputValue directionBase)
     {
-        Vector3 directionAvecVitesse = directionBase.Get<Vector3>() * _vitessePromenade;
-        directionInput = new Vector3(0f,directionAvecVitesse.z , directionAvecVitesse.y);
+        Vector3 directionAvecVitesse = directionBase.Get<Vector2>() * _vitessePromenade;
+        directionInput = new Vector3(0f,directionAvecVitesse.x , directionAvecVitesse.y);
+        Debug.Log(directionInput);
     }
 
     void FixedUpdate()
     {
         // calculer et appliquer la translation
         Vector3 mouvement = directionInput;
+        _rb.AddRelativeForce(mouvement, ForceMode.VelocityChange);
+
+        Vector3 vitesse = new Vector3(0f, 0f , _rb.velocity.z);
+        Vector3 vitessePetite = new Vector3(0f, _rb.velocity.y, 0f);
+        _animator.SetFloat("Vitesse", vitesse.z);
+        _animator.SetFloat("Vitesse petite", vitessePetite.y);
     }
 
     // Update is called once per frame
